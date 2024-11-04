083711dec10185593a6d6561813a1aec33550f59
Mykola Kharechko <crchemist@gmail.com>
2024-11-04 09:18:37 +0200

    fixed issue with div 0

--- xlsx_export/xlsx_export.py
+++ xlsx_export/xlsx_export.py
@@ -272,7 +272,7 @@ def export_reportings_price_report(ws, reportings, date, invoices, start_date):
                         write_off = fal.write_off(outcome-write_off_total)
                         write_off_total += write_off['amount']
                         write_off_price += write_off['price']
-                        if write_off['amount']/write_off['price'] > 60:
+                        if write_off['price'] and (write_off['amount']/write_off['price'] > 60):
                             print(f'Invoice({fal.fal_type}) {fal.doc.number} looks incorrect')
                         if write_off_total >= outcome:
                             next_fre = True
